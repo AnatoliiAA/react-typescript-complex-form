0ad9a71607032347494cd9e8e12151d90b3d8217
Anatolii Abaturov <aabat@softserveinc.com>
2022-02-10 13:45:38 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index_bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ["", ".js", ".ts", ".tsx"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "/public"),
+    },
+    open: true,
+    compress: true,
+    port: 3000,
+  },
+};
